d2823f3b3a336966a70be72d42897b678b2d7f59
Pranav <pranavangrishmusic@gmail.com>
2024-05-06 05:49:53 +0530

    Hello

---
+++ models/modelData.js
@@ -0,0 +1,306 @@
+db.campgrounds.insertMany([
+    {
+      title: "Sukhna Lake",
+      location: "Chandigarh, India",
+      description: "A serene and picturesque lake surrounded by lush gardens, perfect for leisurely strolls and outdoor activities.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7448, 76.8078]
+      },
+      price: 0
+    },
+    {
+      title: "Rock Garden",
+      location: "Chandigarh, India",
+      description: "A unique sculpture garden crafted entirely from industrial and urban waste materials, showcasing the creativity and vision of its creator, Nek Chand.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7597, 76.7873]
+      },
+      price: 30
+    },
+    {
+      title: "Nek Chand Rock Garden",
+      location: "Chandigarh, India",
+      description: "A whimsical and surreal sculpture garden made entirely from recycled materials, showcasing the creativity and vision of its creator, Nek Chand.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7597, 76.7873]
+      },
+      price: 30
+    },
+    {
+      title: "Capitol Complex",
+      location: "Chandigarh, India",
+      description: "A UNESCO World Heritage Site and an architectural masterpiece designed by renowned architect Le Corbusier, featuring the iconic Open Hand Monument.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7408, 76.8072]
+      },
+      price: 0
+    },
+    {
+      title: "Pinjore Gardens",
+      location: "Pinjore, Chandigarh, India",
+      description: "A beautiful Mughal-era garden complex featuring meticulously designed terraces, fountains, and water channels, perfect for a serene day out.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.8083, 76.9244]
+      },
+      price: 20
+    },
+    {
+      title: "Chandigarh Rose Garden",
+      location: "Chandigarh, India",
+      description: "A breathtaking rose garden boasting over 1,600 varieties of roses, offering a stunning display of colors and fragrances.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7369, 76.7878]
+      },
+      price: 10
+    },
+    {
+      title: "Zakir Hussain Rose Garden",
+      location: "Chandigarh, India",
+      description: "Named after the former President of India, this rose garden features over 50,000 rose plants of different varieties, creating a vibrant and fragrant oasis.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7477, 76.7989]
+      },
+      price: 10
+    },
+    {
+      title: "Chandi Mandir",
+      location: "Chandigarh, India",
+      description: "An ancient Hindu temple dedicated to the goddess Chandi, known for its intricate carvings and peaceful ambiance.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7333, 76.7889]
+      },
+      price: 0
+    },
+    {
+      title: "Butterfly Park",
+      location: "Chandigarh, India",
+      description: "A unique park designed to attract and house a variety of butterfly species, offering a serene and educational experience for nature lovers.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7526, 76.7873]
+      },
+      price: 20
+    },
+    {
+      title: "Leisure Valley",
+      location: "Chandigarh, India",
+      description: "A sprawling park and recreational area featuring a variety of attractions such as gardens, amusement rides, and water sports facilities.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7392, 76.7964]
+      },
+      price: 50
+    },
+    {
+      title: "Chandigarh Museum",
+      location: "Chandigarh, India",
+      description: "A fascinating museum showcasing the history, culture, and art of the region, featuring a vast collection of artifacts and exhibits.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7408, 76.8072]
+      },
+      price: 30
+    },
+    {
+      title: "Nawanshahr Bird Sanctuary",
+      location: "Nawanshahr, Chandigarh, India",
+      description: "A serene bird sanctuary that provides a safe haven for a diverse range of migratory and resident bird species, perfect for birdwatching and nature photography.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.9197, 76.1403]
+      },
+      price: 50
+    },
+    {
+      title: "Garden of Fragrance",
+      location: "Chandigarh, India",
+      description: "A unique garden designed to stimulate the sense of smell, featuring a wide variety of fragrant plants and flowers.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7369, 76.7878]
+      },
+      price: 10
+    },
+    {
+      title: "Pardes Shivalik Valley",
+      location: "Panchkula, Chandigarh, India",
+      description: "A picturesque eco-tourism destination nestled in the Shivalik Hills, offering stunning views, nature trails, and adventure activities.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.6978, 76.8544]
+      },
+      price: 80
+    },
+    {
+      title: "Cactus Garden",
+      location: "Chandigarh, India",
+      description: "A unique garden showcasing a diverse collection of cactus and succulent plants from around the world, perfect for nature lovers and plant enthusiasts.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7477, 76.7989]
+      },
+      price: 20
+    },
+    {
+      title: "Japanese Garden",
+      location: "Chandigarh, India",
+      description: "A serene and tranquil Japanese-style garden featuring traditional elements like pagodas, ponds, and carefully manicured landscapes.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7369, 76.7878]
+      },
+      price: 10
+    },
+    {
+      title: "Chandigarh Bird Park",
+      location: "Chandigarh, India",
+      description: "A dedicated bird park housing a wide variety of birds in their natural habitats, providing an opportunity for birdwatching and education.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7526, 76.7873]
+      },
+      price: 30
+    },
+    {
+      title: "Garden of Palms",
+      location: "Chandigarh, India",
+      description: "A lush and tropical garden featuring a diverse collection of palm trees from around the world, creating a serene and relaxing atmosphere.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7477, 76.7989]
+      },
+      price: 10
+    },
+    {
+      title: "Gurudwara Nada Sahib",
+      location: "Chandigarh, India",
+      description: "An important Sikh pilgrimage site and gurudwara, known for its peaceful ambiance and architectural beauty.",
+      geometry: {
+        type: "Point",
+        coordinates: [30.7333, 76.7889]
+      },
+      price: 0
+    },
+    {
+        title: "Gurudwara Nada Sahib",
+        location: "Chandigarh, India",
+        description: "An important Sikh pilgrimage site and gurudwara, known for its peaceful ambiance and architectural beauty.",
+        geometry: {
+          type: "Point",
+          coordinates: [30.7333, 76.7889]
+        },
+        price: 0
+      },
+      {
+        title: "Chandigarh Nature Trail",
+        location: "Chandigarh, India",
+        description: "A network of scenic hiking trails winding through lush forests and natural landscapes, offering a chance to connect with nature and enjoy outdoor activities.",
+        geometry: {
+          type: "Point",
+          coordinates: [30.7526, 76.7873]
+        },
+        price: 20
+      },
+      {
+        title: "Chandigarh Botanical Garden",
+        location: "Chandigarh, India",
+        description: "A vast and meticulously maintained botanical garden showcasing a diverse collection of plant species from around the world, perfect for nature lovers and horticulturists.",
+        geometry: {
+          type: "Point",
+          coordinates: [30.7477, 76.7989]
+        },
+        price: 30
+      },
+      {
+        title: "Bamboo Valley",
+        location: "Chandigarh, India",
+        description: "A tranquil and serene valley filled with lush bamboo groves, offering a peaceful escape from the city and a chance to connect with nature.",
+        geometry: {
+          type: "Point",
+          coordinates: [30.7369, 76.7878]
+        },
+        price: 10
+      },
+      {
+        title: "Chandigarh Golf Club",
+        location: "Chandigarh, India",
+        description: "A premier golf club and course offering a challenging and scenic golfing experience amid lush greenery and picturesque landscapes.",
+        geometry: {
+          type: "Point",
+          coordinates: [30.7526, 76.7873]
+        },
+        price: 100
+      },
+      {
+        title: "Parrot Bird Sanctuary",
+        location: "Chandigarh, India",
+        description: "A dedicated sanctuary for protecting and conserving parrot species, offering visitors a chance to observe these colorful birds in their natural habitat.",
+        geometry: {
+          type: "Point",
+          coordinates: [30.7477, 76.7989]
+        },
+        price: 30
+      },
+      {
+        title: "Chandigarh Lake Club",
+        location: "Chandigarh, India",
+        description: "A premier recreational club offering a range of water sports activities and facilities on the serene Sukhna Lake.",
+        geometry: {
+          type: "Point",
+          coordinates: [30.7448, 76.8078]
+        },
+        price: 50
+      },
+      {
+        title: "Mirzapur Forest Trail",
+        location: "Chandigarh, India",
+        description: "A picturesque forest trail winding through dense woodlands and offering stunning views of the surrounding hills and valleys.",
+        geometry: {
+          type: "Point",
+          coordinates: [30.7597, 76.7873]
+        },
+        price: 20
+      },
+      {
+        title: "Chandigarh War Memorial",
+        location: "Chandigarh, India",
+        description: "A solemn memorial honoring the brave soldiers who sacrificed their lives for the nation, featuring a poignant design and peaceful surroundings.",
+        geometry: {
+          type: "Point",
+          coordinates: [30.7408, 76.8072]
+        },
+        price: 0
+      },
+      {
+        title: "Chandigarh Carnival",
+        location: "Chandigarh, India",
+        description: "An annual cultural festival celebrating the vibrant traditions and diversity of Chandigarh, featuring music, dance, food, and entertainment.",
+        geometry: {
+          type: "Point",
+          coordinates: [30.7369, 76.7878]
+        },
+        price: 50
+      }
+      ])
+
+
+      images: [
+        {
+          _id: ObjectId('66375a4d76fb581dedf3a076'),
+          url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+          filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+        },
+        {
+          _id: ObjectId('66375a4d76fb581dedf3a077'),
+          url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
+          filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+        }
+      ]
